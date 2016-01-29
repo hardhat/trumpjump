d805bac2d0c935a7ba99bffbfb6c3d00c594baee
Dale Wick <dale@hubub.com>
2016-01-29 12:01:30 -0500

    Added a new actor template.

---
+++ src/actor.cpp
@@ -0,0 +1,32 @@
+#include <SDL.h>
+#include "actor.h"
+
+Actor::Actor()
+{
+}
+
+Actor::~Actor()
+{
+
+}
+
+void Actor::init()
+{
+
+}
+
+void Actor::update(int elapsed)
+{
+
+}
+
+void Actor::handle(bool down)
+{
+
+}
+
+void Actor::draw(SDL_Renderer *renderer)
+{
+
+
+}
