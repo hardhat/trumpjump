102df1769f02565657f7b11e8b242328a40b614f
Jace K. Kim <jacek.imagest@gmail.com>
2016-01-30 16:22:25 -0500

    Map image loading

--- src/map.cpp
+++ src/map.cpp
@@ -3,11 +3,10 @@
 #include "map.h"
 #include "image.h"
 #include "world.h"
+#include <iostream>
 
 Map::Map()
 {
-    // TODO set this to be configurable
-    NUM_ITEMS = 5;
     // TODO Encapsulate screensize into a singleton / config so we stretch later
     // From global: screensize={640,384};
     w = 640;
@@ -18,15 +17,29 @@ Map::Map()
 
 Map::~Map()
 {
+    delete platformImage;
+    delete potatoImage;
+    delete meatImage;
+    delete babyImage;
+    delete moneyImage;
+}
 
+void Map::loadImages() {
+    // TODO initialize into spritemaps
+    platformImage = new Image( World::getRenderer(), "platform.png");
+    potatoImage = new Image( World::getRenderer(), "small_potatoes.png");
+    meatImage = new Image( World::getRenderer(), "Meatloaf.png");
+    babyImage = new Image( World::getRenderer(), "baby.png");
+    moneyImage = new Image( World::getRenderer(), "Money.png");
+}
+
+void Map::generateObjects() {
 }
 
 void Map::init()
 {
-
-    // TODO initialize images
-    // TODO initialize into spritemaps
-    platformImage = new Image( World::getRenderer(), "data/platform.png" );
+    // Load assets
+    loadImages();
 }
 
 void Map::newGame()
@@ -36,17 +49,20 @@ void Map::newGame()
 
 void Map::draw(SDL_Renderer *renderer)
 {
-    platformImage->draw(renderer, 0, 0);
+    Image *toDraw;
+    // TODO: iterate through game objects
 }
 
-void Map::update(int elapsed)
-{
+void Map::update(int elapsed) { 
+    // data structure - this is a case where a linked list(vector?) would work
 
+    // TODO 1. shift horizontally
+    // TODO 2. determine if still in display
+    // TODO 3. dispose
 }
 
 int Map::collide(int x,int y,int w,int h)
 {
-
     return MAP_SKY;
 }
 
--- src/map.h
+++ src/map.h
@@ -35,24 +35,42 @@ class Map
         /// returns the item
         int collect(int x,int y,int w,int h);
     private:
+        // Load assets
+        void loadImages();
+
+        // apply gravity
+        void updateGravity();
+
+        // Generate map
+        void generateObjects();
+
+        // NOTE: draw in a buffer? as in to prepare?
+        //  May not be necessary; draw() will only have to draw the current area.
+        // void drawBlock();
         int w;
         int h;
         // TODO Create a World object that implements bijection of w/h<->worldW/h
         // TODO Accept with global object
         int worldW;
         int worldH;
-        MapItems *map;
+        MapItems **map;
 
         int left;	// in screen pixels, for the map scrollng
-        int NUM_ITEMS;
         // TODO Use this signiture for sprite map, not individual instances
         // Image *itemsImage;
 
+        // position
+        float px, py;
+        // velocity
+        float vx, vy; // vx is move-to-right; always constant.
+        // accelleration;
+        float ax, ay; 
 
         // Maybe use a lookup table that matches with enum value?
-        Image *itemPotato;
-        Image *itemMeatLoaf;
-        Image *itemSign;
+        Image *potatoImage;
+        Image *meatImage;
+        Image *babyImage;
+        Image *moneyImage;
 
         // Platform image; leave as separate as a filler
         Image *platformImage;
