293580b1cf78d8492d8aaa4dfc5149b5cfabd18d
Dale Wick <dale@hubub.com>
2016-01-29 20:34:25 -0500

    Implemented the font functions.  Should still scale to world coordinate scale when rendering.

--- src/font.cpp
+++ src/font.cpp
@@ -12,21 +12,55 @@ Font::Font()
 
 Font::~Font()
 {
+    for(int i=0;i<3;i++) {
+        if(fontFace[i]) TTF_CloseFont(fontFace[i]);
+        fontFace[i]=0;
+    }
 }
 
 void Font::init()
 {
 	TTF_Init();
 	// Load up all of the fonts we will use.
+    // TODO: scale up to the global scale.
+    fontFace[FF_HEADLINE]=TTF_OpenFont("data/headline.ttf",36);
+    fontFace[FF_BODY]=TTF_OpenFont("data/body.ttf",18);
+    fontFace[FF_SMALL]=TTF_OpenFont("data/small.ttf",14);
 
 }
 
 void Font::extent(int font,const char *message,int &w,int &h)
 {
+        w=0;
+        h=0;
 
+        if(!fontFace[font]) return;
+        TTF_SizeText(fontFace[font],message,&w,&h);
 }
 
-void Font::draw(int font,const char *message,int x,int y)
+void Font::draw(SDL_Renderer *renderer,int font,const char *message,int x,int y)
 {
+        SDL_Surface *surf;
+        SDL_Rect rect;
+        SDL_Color fg={255,255,255,255};
+        if(font==2) fg.b=0;
 
+        if(!fontFace[font]) {
+                static int count=0;
+                if(count<30)
+                        printf("message at %d,%d: '%s'\n",x,y,message);
+                count++;
+                return;
+        }
+        surf=TTF_RenderText_Blended(fontFace[font],message,fg);
+        rect.x=x;
+        rect.y=y;
+        rect.w=surf->w;
+        rect.h=surf->h;
+        //SDL_SetColorKey(surf, SDL_SRCCOLORKEY, 0);
+        SDL_Texture *tex=0;
+        tex=SDL_CreateTextureFromSurface(renderer,surf);
+        SDL_FreeSurface(surf);
+        SDL_RenderCopy(renderer,tex,NULL,&rect);
+        SDL_DestroyTexture(tex);
 }
--- src/font.h
+++ src/font.h
@@ -1,6 +1,8 @@
 #ifndef FONT_H
 #define FONT_H
 
+#include <SDL.h>
+
 enum FontFace {
 	FF_BODY,
 	FF_SMALL,
@@ -14,7 +16,7 @@ public:
 	~Font();
 	static void init();
 	static void extent(int font,const char *messgae,int &w,int &h);
-	static void draw(int font,const char *message,int x,int y);
+	static void draw(SDL_Renderer *renderer,int font,const char *message,int x,int y);
 };
 
 #endif
