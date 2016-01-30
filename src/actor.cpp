fe3e69c74d2c2fa609a5e7da06b1678e0dcce3c2
Dale Wick <dale@hubub.com>
2016-01-30 16:18:49 -0500

    Added the actor basic sprite loading.

--- src/actor.cpp
+++ src/actor.cpp
@@ -1,8 +1,11 @@
 #include <SDL.h>
+#include "world.h"
 #include "actor.h"
 
 Actor::Actor()
 {
+    x=128;
+    y=160;
 }
 
 Actor::~Actor()
@@ -12,7 +15,7 @@ Actor::~Actor()
 
 void Actor::init()
 {
-
+    sprite=new Image(World::getRenderer(),"Trump.png");
 }
 
 void Actor::update(int elapsed)
@@ -27,6 +30,7 @@ void Actor::handle(bool down)
 
 void Actor::draw(SDL_Renderer *renderer)
 {
-
-
+    if(sprite) {
+        sprite->draw(renderer,x,y);
+    }
 }
