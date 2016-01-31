5c642202b5cbf63fb52ea9a5f41abf5f9e21e622
Mana234 <manaman234@gmail.com>
2016-01-30 19:57:38 -0500

    Did Some more work on Sprite Jumping
    No Description Needed

--- src/actor.cpp
+++ src/actor.cpp
@@ -1,12 +1,20 @@
 #include <SDL.h>
 #include "world.h"
 #include "actor.h"
-
+#include "font.h"
 
 Actor::Actor()
 {
     x=128;
     y=160;
+    //To Do Later = Fix this Bool
+    jump=false;
+
+    frame=0;
+
+    frameTimer=150;
+
+    timer=0;
 }
 
 Actor::~Actor()
@@ -21,19 +29,28 @@ void Actor::init()
 
     sprite->setCellSize(32);
 }
-
 void Actor::update(int elapsed)
 {
+    if (jump==true){
+        frame=8;
+    }else{
+        frameTimer-=elapsed;
+       if (frameTimer<=0){
+        frame=0;
+        frameTimer+=150;
+       }
+    }
 }
 
 void Actor::handle(bool down)
 {
-
+jump=down;
 }
 
 void Actor::draw(SDL_Renderer *renderer)
 {
     if(sprite) {
-        sprite->draw(renderer,x,y,0);
+        sprite->draw(renderer,x,y,frame);
     }
+    if (jump==true){Font::draw(renderer, FF_BODY,"hello",0,200);}
 }
